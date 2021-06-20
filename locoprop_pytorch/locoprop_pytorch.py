f79bfd338ebc4ff43795bfa68859e881225f2bc0
rish-16 <mail.rishabh.anand@gmail.com>
2021-06-20 11:35:12 +0800

    Minor changes

--- locoprop_pytorch/locoprop_pytorch.py
+++ locoprop_pytorch/locoprop_pytorch.py
@@ -7,5 +7,13 @@ class LocoProp(autograd.Function):
         super().__init__()
 
     @staticmethod
+    def forward(ctx, input, weight, bias=None):
+        ctx.save_for_backward(input, weight, bias)
+        output = input.mm(weight.t())
+        if bias is not None:
+            output += bias.unsqueeze(0).expand_as(output)
+        return output
+
+    @staticmethod
     def backward(ctx, grad_output):
         pass
